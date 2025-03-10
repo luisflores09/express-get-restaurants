6e64667b8bc901481bdf44eb97e076eb98564030
Luis <iluisflores@yahoo.com>
2025-03-10 13:45:17 -0500

    created GET request route for restaurants

--- src/app.js
+++ src/app.js
@@ -5,7 +5,10 @@ const db = require("../db/connection");
 
 //TODO: Create your GET Request Route Below: 
 
-
+app.get("/restaurants", async (req, res) => {
+    let allRestaurants = await Restaurant.findAll()
+    res.json(allRestaurants)
+})
 
 
 module.exports = app;
